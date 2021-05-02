40fc4766c749940a6fa47ce63398797de44716b7
SkanderGasmi <skandergasmi7@gmail.com>
2021-05-02 03:39:54 +0200

    division des section de la main page avec des includes

---
+++ Views/front/includes/wrapper.php
@@ -0,0 +1,14 @@
+<div id="wrapper">
+        <!-- ****** Header Area  ****** -->
+        <?php include('header.php') ;?>  
+        <!-- ****** Hero Area  ****** -->
+        <?php include('hero.php') ;?>
+        <!-- ****** Quick View Modal Area  ****** -->
+        <?php include('modalViewProduct.php') ;?>        
+        <!-- ****** products Area Start ****** -->
+        <?php include('productsSection.php') ;?>
+        <!-- ****** Footer Area Start ****** -->
+        <?php include('footer.php') ;?>
+        <!-- ****** Footer Area End ****** -->
+    </div>
+    <!-- /.wrapper end -->

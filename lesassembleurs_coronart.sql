401b5956b787778749946c50b8e939e1ca25d798
SkanderGasmi <skandergasmi7@gmail.com>
2021-05-04 03:52:34 +0200

    base de donnees

--- lesassembleurs_coronart.sql
+++ lesassembleurs_coronart.sql
@@ -3,28 +3,26 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 04 mai 2021 à 01:46
+-- Généré le : mar. 04 mai 2021 à 01:50
 -- Version du serveur :  8.0.21
 -- Version de PHP : 7.3.21
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET FOREIGN_KEY_CHECKS=0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données : `lesassembleurs_coronart`
 --
+CREATE DATABASE IF NOT EXISTS `lesassembleurs_coronart` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE lesassembleurs_coronart;
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `adresses`
+--
+-- Création : Dim 25 avr. 2021 à 18:52
 --
 
 DROP TABLE IF EXISTS `adresses`;
@@ -37,10 +35,17 @@ CREATE TABLE IF NOT EXISTS `adresses` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 
+--
+-- Tronquer la table avant d'insérer `adresses`
+--
+
+TRUNCATE TABLE `adresses`;
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `clients`
+--
+-- Création : mar. 04 mai 2021 à 01:34
 --
 
 DROP TABLE IF EXISTS `clients`;
@@ -55,10 +60,17 @@ CREATE TABLE IF NOT EXISTS `clients` (
   KEY `id_adresse` (`id_adresse`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Tronquer la table avant d'insérer `clients`
+--
+
+TRUNCATE TABLE `clients`;
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `cultures`
+--
+-- Création : sam. 24 avr. 2021 à 03:26
 --
 
 DROP TABLE IF EXISTS `cultures`;
@@ -68,10 +80,17 @@ CREATE TABLE IF NOT EXISTS `cultures` (
   PRIMARY KEY (`id_culture`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
+--
+-- Tronquer la table avant d'insérer `cultures`
+--
+
+TRUNCATE TABLE `cultures`;
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `produits`
+--
+-- Création : mar. 04 mai 2021 à 01:34
 --
 
 DROP TABLE IF EXISTS `produits`;
@@ -86,10 +105,17 @@ CREATE TABLE IF NOT EXISTS `produits` (
   KEY `culture` (`culture`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Tronquer la table avant d'insérer `produits`
+--
+
+TRUNCATE TABLE `produits`;
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `reclamations`
+--
+-- Création : mar. 04 mai 2021 à 01:34
 --
 
 DROP TABLE IF EXISTS `reclamations`;
@@ -102,8 +128,10 @@ CREATE TABLE IF NOT EXISTS `reclamations` (
   KEY `id_client` (`id_client`,`id_produit`),
   KEY `id_produit` (`id_produit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Tronquer la table avant d'insérer `reclamations`
+--
+
+TRUNCATE TABLE `reclamations`;SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
